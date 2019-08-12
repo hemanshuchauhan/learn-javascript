62c972d9d5aed7222a7651b7b751396c896cc9f8
Hemanshu Chauhan <hemanshu.chauhan@publicissapient.com>
2019-08-12 19:45:55 +0100

    javascript learning eg projects

---
+++ 2-JS-basics/starter/script.js
@@ -0,0 +1,35 @@
+console.log('Hello Hemanshu');
+
+/*
+let bills = [100, 10, 300];
+console.log('Length of array:' + bills.length)
+console.log('Bills amount' + bills);
+
+var iterator = bills.values();
+console.log('valuesss:' + iterator.next().value)
+console.log('valuessssss:' + iterator.next().value)
+console.log('valuesss:' + iterator.next().value)
+
+console.log('valuesss:' + iterator.next().value)
+console.log('valuesss:' + iterator.next().value)
+
+function tipamount(bill) {
+
+    if (bill < 50) {
+        return .20 * bill
+    }
+
+else if (bill >= 50 && bill <= 200){
+    return .15 * bill
+    }
+
+else if (bill >200){
+    return .10 *bill
+    }
+}
+*/
+
+
+
+
+
